85b376a2d6fa47558ea2cece51b46e219aebd476
Juan Pablo Rocha <jpr.arango10@uniandes.edu.co>
2017-12-08 20:44:08 -0500

    Standalone

--- VideoAndes/src/vos/Filtro.java
+++ VideoAndes/src/vos/Filtro.java
@@ -1,5 +1,20 @@
 package vos;
 
 public class Filtro {
+	
+	private Checks check;
+	
+	
+	
+	public Filtro(Checks check) {
+		this.check = check;
+	}
 
+	public Checks getCheck() {
+		return check;
+	}
+
+	public void setCheck(Checks check) {
+		this.check = check;
+	}
 }

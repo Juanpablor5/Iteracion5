bf4099e75bb54cc00023471c32e1234da34e25c5
JuanCamiloBojaca <30963726+JuanCamiloBojaca@users.noreply.github.com>
2017-12-08 20:45:50 -0500

    sdf

--- VideoAndes/src/vos/Criterio.java
+++ VideoAndes/src/vos/Criterio.java
@@ -1,5 +1,5 @@
 package vos;
 
 public enum Criterio {
-
+	
 }
--- VideoAndes/src/vos/Filtro.java
+++ VideoAndes/src/vos/Filtro.java
@@ -1,5 +1,20 @@
 package vos;
 
 public class Filtro {
-
+	private Criterio criterio;
+	private String valor;
+	
+	
+	public Criterio getCriterio() {
+		return criterio;
+	}
+	public void setCriterio(Criterio criterio) {
+		this.criterio = criterio;
+	}
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
 }
